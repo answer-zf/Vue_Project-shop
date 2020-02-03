9aa58eae87104b54a4a1490ae09ec10a381500d9
answer-zf <feng18255163789@gmail>
2020-02-03 18:27:09 +0800

    end 06

--- src/components/order/Order.vue
+++ src/components/order/Order.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <el-row>
     <!-- 面包屑导航区域 -->
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
@@ -8,7 +8,6 @@
     </el-breadcrumb>
     <!-- 卡片视图 -->
     <el-card>
-      <!-- 对我而言，好人是一个贬义词，一个人一个房间是一种幸福，不想因为年龄而结婚，更不想负了朋友家人对我的关心，哎，回家倒计时中-->
       <el-row>
         <el-col :span="8">
           <el-input placeholder="请输入内容" v-model="searchKey">
@@ -16,11 +15,76 @@
           </el-input>
         </el-col>
       </el-row>
+
+      <!-- 订单列表数据 -->
+      <el-table :data="orderList" border stripe>
+        <el-table-column type="index"></el-table-column>
+        <el-table-column prop="order_number" label="订单编号"></el-table-column>
+        <el-table-column prop="order_price" label="订单价格"></el-table-column>
+        <el-table-column prop="pay_status" label="是否付款">
+          <template v-slot="scope">
+            <el-tag v-if="scope.row.pay_status === '0'" type="danger">未付款</el-tag>
+            <el-tag v-else type="success">已付款</el-tag>
+          </template>
+        </el-table-column>
+        <el-table-column prop="is_send" label="是否发货"></el-table-column>
+        <el-table-column prop="create_time" label="下单时间">
+          <template v-slot="scope">
+            {{ scope.row.create_time | dataFormat }}
+          </template>
+        </el-table-column>
+        <el-table-column label="操作">
+          <template v-slot="scope">
+            <el-button
+              type="primary"
+              icon="el-icon-edit"
+              size="mini"
+              @click="showEditAddressDialog"
+            ></el-button>
+            <el-button type="success" icon="el-icon-location" size="mini"></el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+
+      <!-- 分页功能 -->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total"
+      ></el-pagination>
+
+      <!-- 修改地址对话框 -->
+      <el-dialog title="修改地址" :visible.sync="editAddressDialogVisible" width="50%">
+        <el-form
+          :model="addressForm"
+          :rules="addressFormRules"
+          ref="addressFormRef"
+          label-width="100px"
+        >
+          <el-form-item label="省市区/县" prop="address1">
+            <el-cascader v-model="addressForm.address1" :options="cityData"></el-cascader>
+          </el-form-item>
+          <el-form-item label="详细地址" prop="address2">
+            <el-input v-model="addressForm.address2"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="editAddressDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="editAddressDialogVisible = false">确 定</el-button>
+        </span>
+      </el-dialog>
     </el-card>
-  </div>
+  </el-row>
 </template>
 
 <script>
+// 导入城市数据信息
+import cityData from './citydata.js'
+
 export default {
   data() {
     return {
@@ -28,19 +92,51 @@ export default {
       queryInfo: {
         query: '',
         pagenum: 1,
-        pagesize: 10,
-        user_id: '',
-        pay_status: '',
-        is_send: '',
-        order_fapiao_title: '',
-        order_fapiao_company: '',
-        order_fapiao_content: '',
-        consignee_addr: ''
-      }
+        pagesize: 10
+      },
+      total: 0,
+      orderList: [],
+      // 控制修改地址对话框的显示和隐藏
+      editAddressDialogVisible: false,
+      // 修改地址对话框表单数据对象
+      addressForm: {
+        address1: [],
+        address2: ''
+      },
+      // 修改地址对话框表单验证规则
+      addressFormRules: {
+        address1: [{ required: true, message: '选择省市区/县', trigger: 'blur' }],
+        address2: [{ required: true, message: '请填写详细地址', trigger: 'blur' }]
+      },
+      cityData
     }
   },
-  created() {},
-  methods: {}
+  created() {
+    this.getOrderList()
+  },
+  methods: {
+    async getOrderList() {
+      const { data: res } = await this.$http.get('orders', {
+        params: this.queryInfo
+      })
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+
+      this.orderList = res.data.goods
+      this.total = res.data.total
+    },
+    handleSizeChange(newSize) {
+      this.queryInfo.pagesize = newSize
+      this.getOrderList()
+    },
+    handleCurrentChange(newPage) {
+      this.queryInfo.pagenum = newPage
+      this.getOrderList()
+    },
+    // 展示修改地址对话框
+    showEditAddressDialog() {
+      this.editAddressDialogVisible = true
+    }
+  }
 }
 </script>
 
